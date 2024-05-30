632c78df379a886bcdaaf9ee7fbaac35bb1e7d0e
y.x.chan <46280186+littlecorsican@users.noreply.github.com>
2024-05-30 14:06:36 +0800

    chore: add comment

--- apps/web/instrumentation.node.ts
+++ apps/web/instrumentation.node.ts
@@ -5,6 +5,10 @@ import {
   SEMRESATTRS_SERVICE_NAME,
 } from '@opentelemetry/semantic-conventions';
 
+/**
+* This file contains the code for open telemetry stack tracing
+*/
+
 const sdk = new NodeSDK({
   resource: new Resource({
     [SEMRESATTRS_SERVICE_NAME]: 'otel-logger',
