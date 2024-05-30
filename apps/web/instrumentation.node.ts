f310017998195cda00e79a2fe56796f55bfc6665
PaanJoe <farhan.gunz@gmail.com>
2024-05-30 15:45:00 +0800

    lint code

--- apps/web/instrumentation.node.ts
+++ apps/web/instrumentation.node.ts
@@ -1,17 +1,15 @@
-import { NodeSDK } from '@opentelemetry/sdk-node';
-import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-node';
-import { Resource } from '@opentelemetry/resources';
-import {
-  SEMRESATTRS_SERVICE_NAME,
-} from '@opentelemetry/semantic-conventions';
+import { Resource } from "@opentelemetry/resources";
+import { NodeSDK } from "@opentelemetry/sdk-node";
+import { ConsoleSpanExporter } from "@opentelemetry/sdk-trace-node";
+import { SEMRESATTRS_SERVICE_NAME } from "@opentelemetry/semantic-conventions";
 
 /**
-* This file contains the code for open telemetry stack tracing
-*/
+ * This file contains the code for open telemetry stack tracing
+ */
 
 const sdk = new NodeSDK({
   resource: new Resource({
-    [SEMRESATTRS_SERVICE_NAME]: 'otel-logger',
+    [SEMRESATTRS_SERVICE_NAME]: "otel-logger",
   }),
   traceExporter: new ConsoleSpanExporter(),
 });
