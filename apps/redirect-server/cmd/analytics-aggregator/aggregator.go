8eb0b796c103659a5e3fd16cb919fc0efa5781a2
Daniel Zairin <danielzairink@gmail.com>
2024-07-16 11:24:05 +0800

    Run aggregator every 5 minutes

---
+++ apps/redirect-server/cmd/analytics-aggregator/aggregator.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"os"
+	"redirect-server/repository/es"
+	"time"
+)
+
+type Aggregator struct {
+	OffsetPath           string
+	RedirectMetadataRepo *es.RedirectMetadataRepo
+	KafkaProducer        *KafkaProducer
+}
+
+func (a *Aggregator) Run(ctx context.Context) error {
+	now := time.Now()
+	shortDate := now.Format("2006-01-02")
+	offset, err := a.getOffset()
+	if err != nil {
+		return err
+	}
+
+	slog.Info("getting redirect metadata")
+	redirectMetadata, err := a.RedirectMetadataRepo.GetRedirectMetadata(ctx, offset, now)
+	if err != nil {
+		return err
+	}
+
+	slog.Info("aggregating redirect metadata")
+	linkAnalytics, err := aggregateRedirectMetadata(redirectMetadata, shortDate)
+	if err != nil {
+		return err
+	}
+
+	slog.Info("sending link analytics to Kafka")
+	err = a.KafkaProducer.SendLinkAnalytics(ctx, linkAnalytics)
+	if err != nil {
+		return err
+	}
+
+	err = a.saveOffset(now)
+	if err != nil {
+		return err
+	}
+
+	slog.Info("analytics sent", slog.String("shortDate", shortDate))
+	return nil
+}
+
+func (a *Aggregator) getOffset() (time.Time, error) {
+	data, err := os.ReadFile(a.OffsetPath)
+	if os.IsNotExist(err) {
+		// If offset not found, use the time five minutes ago
+		fiveMinutesAgo := time.Now().Add(-5 * time.Minute)
+		return fiveMinutesAgo, nil
+	}
+	if err != nil {
+		return time.Time{}, err
+	}
+
+	t, err := time.Parse(time.RFC3339, string(data))
+	if err != nil {
+		return time.Time{}, err
+	}
+
+	return t, nil
+}
+
+func (a *Aggregator) saveOffset(t time.Time) error {
+	err := os.WriteFile(a.OffsetPath, []byte(t.Format(time.RFC3339)), 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- apps/redirect-server/cmd/analytics-aggregator/main.go
+++ apps/redirect-server/cmd/analytics-aggregator/main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"flag"
 	"log/slog"
-	"os"
 	"redirect-server/repository/es"
 	"time"
 )
@@ -13,76 +12,33 @@ func main() {
 	var kafkaAddr string
 	var kafkaTopic string
 	var offsetPath string
-	flag.StringVar(&kafkaAddr, "kafka-addr", "localhost:9092", "Kafka address")
-	flag.StringVar(&kafkaTopic, "kafka-topic", "link-analytics", "Kafka topic")
-	flag.StringVar(&offsetPath, "offset-path", "./analytics-aggregator-offset", "Analytics aggregator offset")
+	{
+		flag.StringVar(&kafkaAddr, "kafka-addr", "localhost:9092", "Kafka address")
+		flag.StringVar(&kafkaTopic, "kafka-topic", "link-analytics", "Kafka topic")
+		flag.StringVar(&offsetPath, "offset-path", "./analytics-aggregator-offset", "Analytics aggregator offset")
+	}
 	flag.Parse()
 
 	ctx := context.Background()
-	redirectMetadataRepo := es.NewRedirectMetadataRepo()
 
-	now := time.Now()
-	shortDate := now.Format("2006-01-02")
-	offset, err := getOffset(offsetPath)
-	if err != nil {
-		panic(err)
-	}
-	if offset == nil {
-		fiveMinutesAgo := now.Add(-5 * time.Minute)
-		offset = &fiveMinutesAgo
+	kafkaProducer := NewKafkaProducer(kafkaAddr, kafkaTopic)
+	defer kafkaProducer.Close()
+
+	aggregator := &Aggregator{
+		OffsetPath:           offsetPath,
+		KafkaProducer:        kafkaProducer,
+		RedirectMetadataRepo: es.NewRedirectMetadataRepo(),
 	}
 
-	kp := NewKafkaProducer(kafkaAddr, kafkaTopic)
-	defer kp.Close()
+	// Run the aggregator every 5 minutes
+	ticker := time.NewTicker(5 * time.Minute)
+	defer ticker.Stop()
 
-	slog.Info("getting redirect metadata")
-	redirectMetadata, err := redirectMetadataRepo.GetRedirectMetadata(ctx, *offset, now)
-	if err != nil {
-		panic(err)
+	for {
+		err := aggregator.Run(ctx)
+		if err != nil {
+			slog.Error("worker run failed", slog.String("errorMessage", err.Error()))
+		}
+		<-ticker.C
 	}
-
-	slog.Info("aggregating redirect metadata")
-	linkAnalytics, err := aggregateRedirectMetadata(redirectMetadata, shortDate)
-	if err != nil {
-		panic(err)
-	}
-
-	slog.Info("sending aggregated metadata to Kafka")
-	err = kp.SendLinkAnalytics(ctx, linkAnalytics)
-	if err != nil {
-		panic(err)
-	}
-
-	err = saveOffset(offsetPath, now)
-	if err != nil {
-		panic(err)
-	}
-
-	slog.Info("analytics sent", slog.String("shortDate", shortDate))
-}
-
-func getOffset(offsetPath string) (*time.Time, error) {
-	data, err := os.ReadFile(offsetPath)
-	if os.IsNotExist(err) {
-		return nil, nil
-	}
-	if err != nil {
-		return nil, err
-	}
-
-	t, err := time.Parse(time.RFC3339, string(data))
-	if err != nil {
-		return nil, err
-	}
-
-	return &t, nil
-}
-
-func saveOffset(offsetPath string, t time.Time) error {
-	err := os.WriteFile(offsetPath, []byte(t.Format(time.RFC3339)), 0644)
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
