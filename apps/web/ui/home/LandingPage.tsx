5d3b001f477880c781ba70e234fa78111adddb5d
PaanJoe <farhan.gunz@gmail.com>
2024-07-18 01:26:46 +0800

    setup home route

---
+++ apps/web/ui/home/LandingPage.tsx
@@ -0,0 +1,12 @@
+// apps/web/components/LandingPage.js
+
+const LandingPage = () => {
+  return (
+    <div>
+      <h1>Welcome to Our Landing Page</h1>
+      <p>This is a static landing page for all users.</p>
+    </div>
+  );
+};
+
+export default LandingPage;
