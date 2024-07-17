e125c93f55de64125ad2075dd2b67d4fdb8caa31
Daniel Zairin <danielzairink@gmail.com>
2024-07-17 16:02:54 +0800

    Fix metadata slice, add omitempty to fields

--- apps/redirect-server/repository/redirect_metadata.go
+++ apps/redirect-server/repository/redirect_metadata.go
@@ -4,13 +4,15 @@ import "time"
 
 type RedirectMetadata struct {
 	LinkID          string    `json:"linkId"`
-	CountryCode     string    `json:"countryCode"`
-	Latitude        float32   `json:"latitude"`
-	Longitude       float32   `json:"longitude"`
-	DeviceType      string    `json:"deviceType"`
-	Browser         string    `json:"browser"`
-	OperatingSystem string    `json:"operatingSystem"`
-	IPAddress       string    `json:"ipAddress"`
-	Referer         string    `json:"referer"`
+	LinkSlug        string    `json:"linkSlug"`
+	LinkURL         string    `json:"linkUrl"`
+	CountryCode     string    `json:"countryCode,omitempty"`
+	Latitude        float32   `json:"latitude,omitempty"`
+	Longitude       float32   `json:"longitude,omitempty"`
+	DeviceType      string    `json:"deviceType,omitempty"`
+	Browser         string    `json:"browser,omitempty"`
+	OperatingSystem string    `json:"operatingSystem,omitempty"`
+	IPAddress       string    `json:"ipAddress,omitempty"`
+	Referer         string    `json:"referer,omitempty"`
 	Timestamp       time.Time `json:"timestamp"`
 }
