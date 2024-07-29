b71a14985ddd61dfebf0830b4b7e70eab05e422a
yxchandev <chanyuxiang6@gmail.com>
2024-07-29 21:10:10 +0800

    Update prettier.config.js

--- prettier.config.js
+++ prettier.config.js
@@ -8,6 +8,6 @@ module.exports = {
   plugins: [
     // comment for better diff
     "prettier-plugin-organize-imports",
-    "prettier-plugin-tailwindcss",
+    "prettier-plugin-tailwindcss", 
   ],
 };
