53593eb82d6cc5c020ece29a06ad2035f5e81ffe
PaanJoe <farhan.gunz@gmail.com>
2024-07-31 01:14:07 +0800

    Update landing page design

---
+++ apps/web/ui/home/AboutSection.tsx
@@ -0,0 +1,69 @@
+export default function AboutSection() {
+  return (
+    <>
+      <section className="border-x-2 border-gray-100 bg-white lg:mx-24 dark:bg-gray-900">
+        <div className="mx-auto max-w-screen-xl px-4 py-8 lg:py-16">
+          <h1 className="mb-4 text-center text-xl font-semibold leading-none tracking-wide text-gray-900 md:text-xl lg:text-3xl dark:text-white">
+            Created for Public Officers
+          </h1>
+          <p className="text-md lg:text-md text-center font-normal text-gray-500 sm:px-16 lg:px-48 dark:text-gray-400">
+            with official email from{" "}
+            <span className="mr-1 rounded-lg bg-gray-100 px-2.5 py-1 text-xs font-medium text-gray-500 dark:bg-gray-900 dark:text-gray-300">
+              @mohe
+            </span>
+            .gov.my
+          </p>
+
+          <div className="mx-auto max-w-screen-xl px-4 py-8 lg:py-16">
+            <div className="grid gap-8 md:grid-cols-2">
+              <div className="rounded-lg border border-gray-200 bg-white p-8 md:p-12 dark:border-gray-700 dark:bg-gray-800">
+                <h2 className="mb-2 text-3xl font-extrabold text-gray-900 dark:text-white">
+                  Placeholder
+                </h2>
+                <span className="rounded-lg border border-gray-50 bg-gray-100 px-2.5 py-1 text-xs font-medium text-gray-500 dark:bg-gray-900 dark:text-gray-300">
+                  CUSTOMIZED
+                </span>
+                <p className="text-md mt-2 text-left font-normal text-gray-800 dark:text-gray-400">
+                  Share short links to save space and for easy recall
+                </p>
+              </div>
+              <div className="rounded-lg border border-gray-200 bg-white p-8 md:p-12 dark:border-gray-700 dark:bg-gray-800">
+                <h2 className="mb-2 text-3xl font-extrabold text-gray-900 dark:text-white">
+                  Placeholder
+                </h2>
+                <span className="rounded-lg border border-gray-50 bg-gray-100 px-2.5 py-1 text-xs font-medium text-gray-500 dark:bg-gray-900 dark:text-gray-300">
+                  ANTI-PHISHING
+                </span>
+                <p className="text-md mt-2 text-left font-normal text-gray-800 dark:text-gray-400">
+                  Use go.gov.my to shorten and secured government links
+                </p>
+              </div>
+              <div className="ounded-lg border border-gray-200 bg-white p-8 md:p-12 dark:border-gray-700 dark:bg-gray-800">
+                <h2 className="mb-2 text-3xl font-extrabold text-gray-900 dark:text-white">
+                  Placeholder
+                </h2>
+                <span className="rounded-lg border border-gray-50 bg-gray-100 px-2.5 py-1 text-xs font-medium text-gray-500 dark:bg-gray-900 dark:text-gray-300">
+                  SOCIAL-SHARING
+                </span>
+                <p className="text-md mt-2 text-left font-normal text-gray-800 dark:text-gray-400">
+                  Upload your own image and share it on social platforms
+                </p>
+              </div>
+              <div className="rounded-lg border border-gray-200 bg-white p-8 md:p-12 dark:border-gray-700 dark:bg-gray-800">
+                <h2 className="mb-2 text-3xl font-extrabold text-gray-900 dark:text-white">
+                  Placeholder
+                </h2>
+                <span className="rounded-lg border border-gray-50 bg-gray-100 px-2.5 py-1 text-xs font-medium text-gray-500 dark:bg-gray-900 dark:text-gray-300">
+                  ANALYTICS
+                </span>
+                <p className="text-md mt-2 text-left font-normal text-gray-800 dark:text-gray-400">
+                  Track your link&apos;s click rate easily through dashboard
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
--- apps/web/ui/home/LandingPage.tsx
+++ apps/web/ui/home/LandingPage.tsx
@@ -1,7 +1,9 @@
 // apps/web/components/LandingPage.js
 
 import IdentifyWebsite from "../header/identify-website";
+import AboutSection from "./AboutSection";
 import HomeNavBar from "./HomeNavBar";
+import Jumbotron from "./Jumbotron";
 
 export default function LandingPage() {
   // { session }: { session: any }
@@ -13,327 +15,17 @@ export default function LandingPage() {
       <HomeNavBar
       // session={session}
       />
-      <section className="bg-white dark:bg-gray-900">
-        <div className="mx-auto max-w-screen-xl px-4 py-8 lg:py-16">
-          <div className="grid gap-8 md:grid-cols-2">
-            <div className="">
-              <h2 className="mb-2 text-5xl font-semibold text-gray-900 dark:text-white">
-                Trusted Official Links for Malaysians
-              </h2>
-              <p className="mb-4 text-lg font-normal text-gray-500 dark:text-gray-400">
-                <span className="mr-1 rounded-lg bg-blue-100 px-2.5 py-1 text-xs font-medium text-blue-500 dark:bg-blue-900 dark:text-blue-300">
-                  go.gov.my
-                </span>{" "}
-                short links can only be created by public officers, so you can
-                be sure it&apos;s from a trustworthy source.
-              </p>
-              <p className="mb-4 text-lg font-normal text-gray-500 dark:text-gray-400">
-                Are you a public officer? &nbsp;
-                <a
-                  href="#"
-                  className="inline-flex items-center text-lg font-medium text-blue-600 hover:underline dark:text-blue-500"
-                >
-                  Sign In Now
-                  <svg
-                    className="ms-2 h-3.5 w-3.5 rtl:rotate-180"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 14 10"
-                  >
-                    <path
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M1 5h12m0 0L9 1m4 4L9 9"
-                    />
-                  </svg>
-                </a>
-              </p>
-            </div>
-            <div className="rounded-lg border border-gray-200 bg-gray-50 p-8 dark:border-gray-700 dark:bg-gray-800 md:p-12">
-              <a
-                href="#"
-                className="mb-2 inline-flex items-center rounded-md bg-purple-100 px-2.5 py-0.5 text-xs font-medium text-purple-800 dark:bg-gray-700 dark:text-purple-400"
-              >
-                <svg
-                  className="me-1.5 h-2.5 w-2.5"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 20 16"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M5 4 1 8l4 4m10-8 4 4-4 4M11 1 9 15"
-                  />
-                </svg>
-                Code
-              </a>
-              <h2 className="mb-2 text-3xl font-extrabold text-gray-900 dark:text-white">
-                Best react libraries around the web
-              </h2>
-              <p className="mb-4 text-lg font-normal text-gray-500 dark:text-gray-400">
-                Static websites are now used to bootstrap lots of websites and
-                are becoming the basis for a variety of tools that even
-                influence both web designers and developers.
-              </p>
-              <a
-                href="#"
-                className="inline-flex items-center text-lg font-medium text-blue-600 hover:underline dark:text-blue-500"
-              >
-                Read more
-                <svg
-                  className="ms-2 h-3.5 w-3.5 rtl:rotate-180"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 14 10"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M1 5h12m0 0L9 1m4 4L9 9"
-                  />
-                </svg>
-              </a>
-            </div>
-          </div>
-        </div>
-      </section>
+      <Jumbotron />
+      <AboutSection />
 
       <section className="bg-white dark:bg-gray-900">
         <div className="mx-auto max-w-screen-xl px-4 py-8 text-center lg:py-16">
-          <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
-            Created for Public Officers
-          </h1>
-          <p className="mb-8 text-lg font-normal text-gray-500 dark:text-gray-400 sm:px-16 lg:px-48 lg:text-xl">
-            with official email from{" "}
-            <span className="mr-1 rounded-lg bg-blue-100 px-2.5 py-1 text-xs font-medium text-blue-500 dark:bg-blue-900 dark:text-blue-300">
-              @mohe
-            </span>
-            .gov.my
-          </p>
-
-          <div className="mx-auto max-w-screen-xl px-4 py-8 lg:py-16">
-            <div className="my-5 grid gap-8 md:grid-cols-2">
-              <div className="rounded-lg border border-gray-200 bg-gray-50 p-8 dark:border-gray-700 dark:bg-gray-800 md:p-12">
-                <a
-                  href="#"
-                  className="mb-2 inline-flex items-center rounded-md bg-green-100 px-2.5 py-0.5 text-xs font-medium text-green-800 dark:bg-gray-700 dark:text-green-400"
-                >
-                  <svg
-                    className="me-1.5 h-2.5 w-2.5"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    viewBox="0 0 18 18"
-                  >
-                    <path d="M17 11h-2.722L8 17.278a5.512 5.512 0 0 1-.9.722H17a1 1 0 0 0 1-1v-5a1 1 0 0 0-1-1ZM6 0H1a1 1 0 0 0-1 1v13.5a3.5 3.5 0 1 0 7 0V1a1 1 0 0 0-1-1ZM3.5 15.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2ZM16.132 4.9 12.6 1.368a1 1 0 0 0-1.414 0L9 3.55v9.9l7.132-7.132a1 1 0 0 0 0-1.418Z" />
-                  </svg>
-                  Design
-                </a>
-                <h2 className="mb-2 text-3xl font-extrabold text-gray-900 dark:text-white">
-                  Start with Flowbite Design System
-                </h2>
-                <p className="mb-4 text-lg font-normal text-gray-500 dark:text-gray-400">
-                  Static websites are now used to bootstrap lots of websites and
-                  are becoming the basis for a variety of tools that even
-                  influence both web designers and developers.
-                </p>
-                <a
-                  href="#"
-                  className="inline-flex items-center text-lg font-medium text-blue-600 hover:underline dark:text-blue-500"
-                >
-                  Read more
-                  <svg
-                    className="ms-2 h-3.5 w-3.5 rtl:rotate-180"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 14 10"
-                  >
-                    <path
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M1 5h12m0 0L9 1m4 4L9 9"
-                    />
-                  </svg>
-                </a>
-              </div>
-              <div className="rounded-lg border border-gray-200 bg-gray-50 p-8 dark:border-gray-700 dark:bg-gray-800 md:p-12">
-                <a
-                  href="#"
-                  className="mb-2 inline-flex items-center rounded-md bg-purple-100 px-2.5 py-0.5 text-xs font-medium text-purple-800 dark:bg-gray-700 dark:text-purple-400"
-                >
-                  <svg
-                    className="me-1.5 h-2.5 w-2.5"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 20 16"
-                  >
-                    <path
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M5 4 1 8l4 4m10-8 4 4-4 4M11 1 9 15"
-                    />
-                  </svg>
-                  Code
-                </a>
-                <h2 className="mb-2 text-3xl font-extrabold text-gray-900 dark:text-white">
-                  Best react libraries around the web
-                </h2>
-                <p className="mb-4 text-lg font-normal text-gray-500 dark:text-gray-400">
-                  Static websites are now used to bootstrap lots of websites and
-                  are becoming the basis for a variety of tools that even
-                  influence both web designers and developers.
-                </p>
-                <a
-                  href="#"
-                  className="inline-flex items-center text-lg font-medium text-blue-600 hover:underline dark:text-blue-500"
-                >
-                  Read more
-                  <svg
-                    className="ms-2 h-3.5 w-3.5 rtl:rotate-180"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 14 10"
-                  >
-                    <path
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M1 5h12m0 0L9 1m4 4L9 9"
-                    />
-                  </svg>
-                </a>
-              </div>
-            </div>
-            <div className="grid gap-8 md:grid-cols-2">
-              <div className="rounded-lg border border-gray-200 bg-gray-50 p-8 dark:border-gray-700 dark:bg-gray-800 md:p-12">
-                <a
-                  href="#"
-                  className="mb-2 inline-flex items-center rounded-md bg-green-100 px-2.5 py-0.5 text-xs font-medium text-green-800 dark:bg-gray-700 dark:text-green-400"
-                >
-                  <svg
-                    className="me-1.5 h-2.5 w-2.5"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    viewBox="0 0 18 18"
-                  >
-                    <path d="M17 11h-2.722L8 17.278a5.512 5.512 0 0 1-.9.722H17a1 1 0 0 0 1-1v-5a1 1 0 0 0-1-1ZM6 0H1a1 1 0 0 0-1 1v13.5a3.5 3.5 0 1 0 7 0V1a1 1 0 0 0-1-1ZM3.5 15.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2ZM16.132 4.9 12.6 1.368a1 1 0 0 0-1.414 0L9 3.55v9.9l7.132-7.132a1 1 0 0 0 0-1.418Z" />
-                  </svg>
-                  Design
-                </a>
-                <h2 className="mb-2 text-3xl font-extrabold text-gray-900 dark:text-white">
-                  Start with Flowbite Design System
-                </h2>
-                <p className="mb-4 text-lg font-normal text-gray-500 dark:text-gray-400">
-                  Static websites are now used to bootstrap lots of websites and
-                  are becoming the basis for a variety of tools that even
-                  influence both web designers and developers.
-                </p>
-                <a
-                  href="#"
-                  className="inline-flex items-center text-lg font-medium text-blue-600 hover:underline dark:text-blue-500"
-                >
-                  Read more
-                  <svg
-                    className="ms-2 h-3.5 w-3.5 rtl:rotate-180"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 14 10"
-                  >
-                    <path
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M1 5h12m0 0L9 1m4 4L9 9"
-                    />
-                  </svg>
-                </a>
-              </div>
-              <div className="rounded-lg border border-gray-200 bg-gray-50 p-8 dark:border-gray-700 dark:bg-gray-800 md:p-12">
-                <a
-                  href="#"
-                  className="mb-2 inline-flex items-center rounded-md bg-purple-100 px-2.5 py-0.5 text-xs font-medium text-purple-800 dark:bg-gray-700 dark:text-purple-400"
-                >
-                  <svg
-                    className="me-1.5 h-2.5 w-2.5"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 20 16"
-                  >
-                    <path
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M5 4 1 8l4 4m10-8 4 4-4 4M11 1 9 15"
-                    />
-                  </svg>
-                  Code
-                </a>
-                <h2 className="mb-2 text-3xl font-extrabold text-gray-900 dark:text-white">
-                  Best react libraries around the web
-                </h2>
-                <p className="mb-4 text-lg font-normal text-gray-500 dark:text-gray-400">
-                  Static websites are now used to bootstrap lots of websites and
-                  are becoming the basis for a variety of tools that even
-                  influence both web designers and developers.
-                </p>
-                <a
-                  href="#"
-                  className="inline-flex items-center text-lg font-medium text-blue-600 hover:underline dark:text-blue-500"
-                >
-                  Read more
-                  <svg
-                    className="ms-2 h-3.5 w-3.5 rtl:rotate-180"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 14 10"
-                  >
-                    <path
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M1 5h12m0 0L9 1m4 4L9 9"
-                    />
-                  </svg>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <section className="bg-white dark:bg-gray-900">
-        <div className="mx-auto max-w-screen-xl px-4 py-8 text-center lg:py-16">
-          <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
+          <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
             Our Performance so far
           </h1>
           <div className="mx-auto max-w-screen-xl px-4 py-8 lg:py-16">
             <div className="my-5 grid gap-8 md:grid-cols-3">
-              <div className="rounded-lg border border-gray-200 bg-gray-50 p-8 dark:border-gray-700 dark:bg-gray-800 md:p-12">
+              <div className="rounded-lg border border-gray-200 bg-gray-50 p-8 md:p-12 dark:border-gray-700 dark:bg-gray-800">
                 <a
                   href="#"
                   className="mb-2 inline-flex items-center rounded-md bg-green-100 px-2.5 py-0.5 text-xs font-medium text-green-800 dark:bg-gray-700 dark:text-green-400"
@@ -379,7 +71,7 @@ export default function LandingPage() {
                   </svg>
                 </a>
               </div>
-              <div className="rounded-lg border border-gray-200 bg-gray-50 p-8 dark:border-gray-700 dark:bg-gray-800 md:p-12">
+              <div className="rounded-lg border border-gray-200 bg-gray-50 p-8 md:p-12 dark:border-gray-700 dark:bg-gray-800">
                 <a
                   href="#"
                   className="mb-2 inline-flex items-center rounded-md bg-green-100 px-2.5 py-0.5 text-xs font-medium text-green-800 dark:bg-gray-700 dark:text-green-400"
@@ -425,7 +117,7 @@ export default function LandingPage() {
                   </svg>
                 </a>
               </div>
-              <div className="rounded-lg border border-gray-200 bg-gray-50 p-8 dark:border-gray-700 dark:bg-gray-800 md:p-12">
+              <div className="rounded-lg border border-gray-200 bg-gray-50 p-8 md:p-12 dark:border-gray-700 dark:bg-gray-800">
                 <a
                   href="#"
                   className="mb-2 inline-flex items-center rounded-md bg-purple-100 px-2.5 py-0.5 text-xs font-medium text-purple-800 dark:bg-gray-700 dark:text-purple-400"
@@ -479,7 +171,7 @@ export default function LandingPage() {
               </div>
             </div>
             <div className="my-5 grid gap-8 md:grid-cols-1">
-              <div className="rounded-lg border border-gray-200 bg-gray-50 p-8 dark:border-gray-700 dark:bg-gray-800 md:p-12">
+              <div className="rounded-lg border border-gray-200 bg-gray-50 p-8 md:p-12 dark:border-gray-700 dark:bg-gray-800">
                 <a
                   href="#"
                   className="mb-2 inline-flex items-center rounded-md bg-green-100 px-2.5 py-0.5 text-xs font-medium text-green-800 dark:bg-gray-700 dark:text-green-400"
@@ -608,9 +300,9 @@ export default function LandingPage() {
               </div>
             </div>
           </div>
-          <hr className="my-6 border-gray-200 dark:border-gray-700 sm:mx-auto lg:my-8" />
+          <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8 dark:border-gray-700" />
           <div className="sm:flex sm:items-center sm:justify-between">
-            <span className="text-sm text-gray-500 dark:text-gray-400 sm:text-center">
+            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
               © 2023{" "}
               <a href="https://flowbite.com/" className="hover:underline">
                 Flowbite™
