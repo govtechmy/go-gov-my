214cf93b6f55973702de9d4ebbbf9d75e3207e0b
yxchandev <chanyuxiang6@gmail.com>
2024-08-02 20:43:26 +0800

    debug

--- apps/web/lib/auth/constants.ts
+++ apps/web/lib/auth/constants.ts
@@ -1,3 +1,3 @@
 export const IS_PRODUCTION = process.env.NODE_ENV === "production";
 
-export const SESSION_TOKEN_NAME = "next-auth.session-token";
+export const SESSION_TOKEN_NAME = "__Secure-next-auth.session-token";
