7b3f03db6a12933b924e85f141c70d78328d151a
yxchandev <chanyuxiang6@gmail.com>
2024-08-02 18:49:09 +0800

    debugging

--- apps/web/lib/middleware/app.ts
+++ apps/web/lib/middleware/app.ts
@@ -15,6 +15,8 @@ export default async function AppMiddleware(req: NextRequest) {
     user?: UserProps;
   };
 
+  console.log("sessionxxx", session)
+
   // Farhan 18 Jul 24
   // This is for temporary to view the static page, need to remove this once deploy
   if (
