b9a9692806bf0542c02e07602406df6b2bfce867
yxchandev <chanyuxiang6@gmail.com>
2024-08-06 19:08:36 +0800

    enable clicks anal on dashboard

--- apps/web/ui/links/link-card.tsx
+++ apps/web/ui/links/link-card.tsx
@@ -117,24 +117,23 @@ export default function LinkCard({
   const isVisible = !!entry?.isIntersecting;
 
   // TODO: Fix analytics API
-  // const { data: clicks } = useSWR<number>(
-  //   // only fetch clicks if the link is visible and there's a slug and the usage is not exceeded
-  //   isVisible &&
-  //     workspaceId &&
-  //     !exceededClicks &&
-  //     `/api/analytics/clicks?workspaceId=${workspaceId}&linkId=${id}&interval=all&`,
-  //   (url) =>
-  //     fetcher(url, {
-  //       headers: {
-  //         "Request-Source": process.env.NEXT_PUBLIC_APP_DOMAIN!,
-  //       },
-  //     }),
-  //   {
-  //     fallbackData: props.clicks,
-  //     dedupingInterval: 60000,
-  //   },
-  // );
-  const clicks = 0;
+  const { data: clicks } = useSWR<number>(
+    // only fetch clicks if the link is visible and there's a slug and the usage is not exceeded
+    isVisible &&
+      workspaceId &&
+      !exceededClicks &&
+      `/api/analytics/clicks?workspaceId=${workspaceId}&linkId=${id}&interval=all&`,
+    (url) =>
+      fetcher(url, {
+        headers: {
+          "Request-Source": process.env.NEXT_PUBLIC_APP_DOMAIN!,
+        },
+      }),
+    {
+      fallbackData: props.clicks,
+      dedupingInterval: 60000,
+    },
+  );
 
   const { setShowLinkQRModal, LinkQRModal } = useLinkQRModal({
     props,
