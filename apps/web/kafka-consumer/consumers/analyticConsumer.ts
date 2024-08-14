91d5ef1636a1a291624888beaaf9dcc7c4fbfcbb
Daniel Zairin <danielzairink@gmail.com>
2024-08-14 16:58:21 +0800

    chore: parse kafka analytics message and some refactoring

--- apps/web/kafka-consumer/consumers/analyticConsumer.ts
+++ apps/web/kafka-consumer/consumers/analyticConsumer.ts
@@ -1,8 +1,10 @@
 import { prisma } from "@/lib/prisma";
+import { AnalyticsMessageSchema } from "kafka-consumer/models/AnalyticsSchema";
+import type { Consumer, Logger } from "kafkajs";
 import { consumeAnalytics, sumTwoObj } from "../utils/analytics";
 import { retryWithDelay } from "../utils/retry";
 
-export async function runAnalyticConsumer(consumer: any, log: any) {
+export async function runAnalyticConsumer(consumer: Consumer, log: Logger) {
   await consumer.subscribe({
     topic: process.env.ANALYTIC_TOPIC!,
     fromBeginning: true,
@@ -11,68 +13,64 @@ export async function runAnalyticConsumer(consumer: any, log: any) {
   await consumer.run({
     autoCommit: false,
     eachMessage: async ({ topic, partition, message }) => {
-      if (!message.value) return;
+      const processAnalyticsMessage = async () => {
+        if (!message.value) return;
 
-      const data = JSON.parse(message?.value?.toString("utf8") || "{}");
-      const aggregatedDate = data?.aggregatedDate;
-      const from = data?.from;
-      const to = data?.to;
+        const data = await AnalyticsMessageSchema.parseAsync(
+          JSON.parse(message.value.toString("utf8") || "{}"),
+        );
+        const { aggregatedDate, from, to } = data;
 
-      const processMessageAnalytics = async () => {
-        data?.linkAnalytics?.forEach(async (link) => {
-          // 1. Get Analytics Rows
-          const row = await prisma.analytics.findMany({
-            where: {
-              AND: [
-                { aggregatedDate: new Date(aggregatedDate) },
-                { linkId: { equals: link?.linkId } },
-              ],
-            },
-            take: 1,
-          });
-
-          // 2. Check if today record already exists
-          if (row.length > 0) {
-            const metaDataFromDb = row[0]?.metadata;
-
-            // Sum metadata
-            const combineMetaData = sumTwoObj(
-              metaDataFromDb,
-              consumeAnalytics(link, aggregatedDate, from, to)?.metadata,
-            );
-
-            // Update records
-            await prisma.analytics.update({
-              where: { id: row[0].id },
-              data: { metadata: combineMetaData, from, to },
-            });
-          } else {
-            // 3. If not exists, create a new record (meaning new day)
-            await prisma.analytics.create({
-              data: consumeAnalytics(link, aggregatedDate, from, to),
-            });
-          }
-
-          // Lets update the link.clicks table
-          if (link?.total && link?.linkId) {
-            // Find the link in the table
-            const linkDb = await prisma.link.findFirst({
-              where: { id: link.linkId },
+        const linkAnalyticsPromises = data.linkAnalytics.map(
+          async (analytics) => {
+            // 1. Get Analytics Rows
+            const row = await prisma.analytics.findMany({
+              where: {
+                AND: [
+                  { aggregatedDate: aggregatedDate },
+                  { linkId: { equals: analytics.linkId } },
+                ],
+              },
+              take: 1,
             });
 
-            // Let kafka retry until success if the link is not found
-            if (linkDb === null || linkDb === undefined) {
-              throw new Error("Link not found in the database");
+            // 2. Check if today record already exists
+            if (row.length > 0) {
+              const metaDataFromDb = row[0]?.metadata;
+
+              // Sum metadata
+              const combineMetaData = sumTwoObj(
+                metaDataFromDb,
+                consumeAnalytics(analytics, new Date(aggregatedDate), from, to)
+                  .metadata,
+              );
+
+              // Update records
+              await prisma.analytics.update({
+                where: { id: row[0].id },
+                data: { metadata: combineMetaData, from, to },
+              });
+            } else {
+              // 3. If not exists, create a new record (meaning new day)
+              await prisma.analytics.create({
+                data: consumeAnalytics(
+                  analytics,
+                  new Date(aggregatedDate),
+                  from,
+                  to,
+                ),
+              });
             }
 
-            const aggregatedClicks = linkDb.clicks + link.total;
-
+            // Increment the link's clicks column
             await prisma.link.update({
-              where: { id: link.linkId },
-              data: { clicks: aggregatedClicks },
+              where: { id: analytics.linkId },
+              data: { clicks: { increment: analytics.total } },
             });
-          }
-        });
+          },
+        );
+
+        await Promise.all(linkAnalyticsPromises);
 
         // Commit offset if success
         await consumer.commitOffsets([
@@ -84,7 +82,7 @@ export async function runAnalyticConsumer(consumer: any, log: any) {
         ]);
       };
 
-      retryWithDelay(processMessageAnalytics);
+      retryWithDelay(processAnalyticsMessage);
     },
   });
 }
