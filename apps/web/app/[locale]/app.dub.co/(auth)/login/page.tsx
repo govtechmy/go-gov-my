5fdd26d040c45742b2aaaf6408354803668f12a5
PaanJoe <38974707+paanjoe@users.noreply.github.com>
2024-09-06 07:00:42 +0800

    chore: Change masthead style and config SEO

--- apps/web/app/[locale]/app.dub.co/(auth)/login/page.tsx
+++ apps/web/app/[locale]/app.dub.co/(auth)/login/page.tsx
@@ -16,30 +16,34 @@ export async function generateMetadata({ params }) {
 export default function LoginPage() {
   const { messages } = useIntlHook();
   return (
-    <div className="relative z-10 mt-[calc(30vh)] h-fit w-full max-w-md overflow-hidden border-y border-gray-200 sm:rounded-2xl sm:border sm:shadow-xl">
-      <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-6 pt-8 text-center sm:px-16">
-        <a href={HOME_DOMAIN}>
-          <Logo className="h-10 w-10" />
-        </a>
-        <h3 className="text-xl font-semibold">
-          {messages?.login?.login_msg_1} {process.env.NEXT_PUBLIC_APP_NAME}
-        </h3>
-        <p className="text-sm text-gray-500">{messages?.login?.login_msg_2}</p>
+    <>
+      <div className="relative z-10 mt-[calc(30vh)] h-fit w-full max-w-md overflow-hidden border-y border-gray-200 sm:rounded-2xl sm:border sm:shadow-xl">
+        <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-6 pt-8 text-center sm:px-16">
+          <a href={HOME_DOMAIN}>
+            <Logo className="h-10 w-10" />
+          </a>
+          <h3 className="text-xl font-semibold">
+            {messages?.login?.login_msg_1} {process.env.NEXT_PUBLIC_APP_NAME}
+          </h3>
+          <p className="text-sm text-gray-500">
+            {messages?.login?.login_msg_2}
+          </p>
+        </div>
+        <div className="flex flex-col space-y-3 bg-gray-50 px-4 py-8 sm:px-16">
+          <Suspense
+            fallback={
+              <>
+                <Button disabled={true} variant="secondary" />
+                <Button disabled={true} variant="secondary" />
+                <Button disabled={true} variant="secondary" />
+                <div className="mx-auto h-5 w-3/4 rounded-lg bg-gray-100" />
+              </>
+            }
+          >
+            <LoginForm />
+          </Suspense>
+        </div>
       </div>
-      <div className="flex flex-col space-y-3 bg-gray-50 px-4 py-8 sm:px-16">
-        <Suspense
-          fallback={
-            <>
-              <Button disabled={true} variant="secondary" />
-              <Button disabled={true} variant="secondary" />
-              <Button disabled={true} variant="secondary" />
-              <div className="mx-auto h-5 w-3/4 rounded-lg bg-gray-100" />
-            </>
-          }
-        >
-          <LoginForm />
-        </Suspense>
-      </div>
-    </div>
+    </>
   );
 }
