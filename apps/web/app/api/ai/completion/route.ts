56212953a8c5baf1e7d90401a3ccdd5d9539f0da
yxchandev <chanyuxiang6@gmail.com>
2024-09-05 10:56:56 +0800

    Revert "silence anthropic error response"
    This reverts commit d1b24259bca13537b47bcfc16c5bd451cf895a29.

--- apps/web/app/api/ai/completion/route.ts
+++ apps/web/app/api/ai/completion/route.ts
@@ -1,11 +1,10 @@
 import { anthropic } from "@/lib/anthropic";
-import { handleAndReturnErrorResponse } from "@/lib/api/errors";
+import { DubApiError, handleAndReturnErrorResponse } from "@/lib/api/errors";
 import { withWorkspaceEdge } from "@/lib/auth/workspace-edge";
 import { prisma } from "@/lib/prisma";
 import z from "@/lib/zod";
 import { waitUntil } from "@vercel/functions";
 import { AnthropicStream, StreamingTextResponse } from "ai";
-import { NextResponse } from "next/server";
 
 const completionSchema = z.object({
   prompt: z.string(),
@@ -20,11 +19,10 @@ export const POST = withWorkspaceEdge(
   async ({ req, workspace }) => {
     if (!anthropic) {
       console.error("Anthropic is not configured. Skipping the request.");
-      return NextResponse.json({});
-      // throw new DubApiError({
-      //   code: "bad_request",
-      //   message: "Anthropic API key is not configured.",
-      // });
+      throw new DubApiError({
+        code: "bad_request",
+        message: "Anthropic API key is not configured.",
+      });
     }
 
     try {
