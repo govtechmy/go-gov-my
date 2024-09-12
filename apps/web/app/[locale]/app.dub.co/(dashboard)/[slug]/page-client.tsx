1f6d6a5ea901316eb1680817b5423c14bb7d90f9
PaanJoe <38974707+paanjoe@users.noreply.github.com>
2024-09-12 16:18:34 +0800

    Export to CSV Feat?

--- apps/web/app/[locale]/app.dub.co/(dashboard)/[slug]/page-client.tsx
+++ apps/web/app/[locale]/app.dub.co/(dashboard)/[slug]/page-client.tsx
@@ -1,13 +1,98 @@
 "use client";
 
 import { useIntlClientHook } from "@/lib/middleware/utils/useI18nClient";
+import useLinks from "@/lib/swr/use-links";
 import LinksContainer from "@/ui/links/links-container";
 import { useAddEditLinkModal } from "@/ui/modals/add-edit-link-modal";
 import { MaxWidthWrapper } from "@dub/ui";
+import { Button } from "@dub/ui/src/button";
+import { saveAs } from "file-saver/dist/FileSaver";
+import { json2csv } from "json-2-csv";
 
 export default function WorkspaceLinksClient() {
   const { AddEditLinkModal, AddEditLinkButton } = useAddEditLinkModal();
   const { messages } = useIntlClientHook();
+  const { links, isValidating } = useLinks();
+
+  const convertToCSV = async (data: object[]) => {
+    const headers = [
+      "id",
+      "domain",
+      "key",
+      "url",
+      "archived",
+      "expiresAt",
+      "expiredUrl",
+      "passwordEnabledAt",
+      "externalId",
+      "banned",
+      "trackConversion",
+      "proxy",
+      "title",
+      "description",
+      "image",
+      "utm_source",
+      "utm_medium",
+      "utm_campaign",
+      "utm_term",
+      "utm_content",
+      "ios",
+      "android",
+      "geo.AL",
+      "userId",
+      "projectId",
+      "publicStats",
+      "clicks",
+      "lastClicked",
+      "createdAt",
+      "updatedAt",
+      "comments",
+      "user.id",
+      "user.name",
+      "user.email",
+      "user.emailVerified",
+      "user.image",
+      "user.agencyCode",
+      "user.role",
+      "user.createdAt",
+      "user.subscribed",
+      "tags",
+      "shortLink",
+      "tagId",
+      "qrCode",
+      "workspaceId",
+    ];
+
+    return json2csv(data, {
+      keys: headers,
+      parseValue(fieldValue, defaultParser) {
+        if (fieldValue instanceof Date) {
+          return fieldValue.toISOString();
+        }
+        return defaultParser(fieldValue);
+      },
+    });
+  };
+
+  const getShortDate = () => {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+    return `${year}${month}${day}`;
+  };
+
+  const exportToCSV = async () => {
+    try {
+      const csv = links ? await convertToCSV(links) : "";
+      const blob = new Blob([csv], { type: "text/csv;charset=utf-8;" });
+      const shortDate = getShortDate();
+      saveAs(blob, `${shortDate}_links.csv`);
+    } catch (error) {
+      console.error("Error exporting to CSV:", error);
+    }
+  };
+
   return (
     <>
       <AddEditLinkModal />
@@ -17,8 +102,13 @@ export default function WorkspaceLinksClient() {
             <h1 className="text-2xl text-gray-600">
               {messages?.dashboard?.Links}
             </h1>
-            <div className="flex">
-              <AddEditLinkButton />
+            <div className="flex gap-2">
+              <div>
+                <AddEditLinkButton />
+              </div>
+              <div>
+                <Button text="Export to CSV" onClick={exportToCSV} />
+              </div>
             </div>
           </div>
         </MaxWidthWrapper>
