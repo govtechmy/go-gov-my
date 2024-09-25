209e774c22bb99b2cd44be080e92e5495a86806a
yxchandev <chanyuxiang6@gmail.com>
2024-09-25 11:46:36 +0800

    add jaeger and prometheus exporter

--- apps/web/instrumentation.node.ts
+++ apps/web/instrumentation.node.ts
@@ -1,3 +1,5 @@
+import { JaegerExporter } from '@opentelemetry/exporter-jaeger';
+import { PrometheusExporter } from '@opentelemetry/exporter-prometheus';
 import { Resource } from '@opentelemetry/resources';
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-node';
@@ -6,6 +8,15 @@ import { SEMRESATTRS_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
 /**
  * This file contains the code for open telemetry stack tracing
  */
+
+const jaegerExporter = new JaegerExporter({
+  endpoint: 'http://localhost:14268/api/traces',
+});
+
+const prometheusExporter = new PrometheusExporter({
+  port: 9464,
+  endpoint: '/metrics',
+});
 
 const sdk = new NodeSDK({
   resource: new Resource({
