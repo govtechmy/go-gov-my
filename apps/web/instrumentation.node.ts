dc8c7eec3889be43d09b49bcec656bb20c03fc63
yxchandev <chanyuxiang6@gmail.com>
2024-09-25 11:59:35 +0800

    integrate prometheus

--- apps/web/instrumentation.node.ts
+++ apps/web/instrumentation.node.ts
@@ -1,6 +1,6 @@
-import { JaegerExporter } from '@opentelemetry/exporter-jaeger';
 import { PrometheusExporter } from '@opentelemetry/exporter-prometheus';
 import { Resource } from '@opentelemetry/resources';
+import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-node';
 import { SEMRESATTRS_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
@@ -9,7 +9,7 @@ import { SEMRESATTRS_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
  * This file contains the code for open telemetry stack tracing
  */
 
-const jaegerExporter = new JaegerExporter({
+const jaegerExporter = new PeriodicExportingMetricReader({
   endpoint: 'http://localhost:14268/api/traces',
 });
 
@@ -18,11 +18,18 @@ const prometheusExporter = new PrometheusExporter({
   endpoint: '/metrics',
 });
 
+const metricReader = new PeriodicExportingMetricReader({
+  exporter: prometheusExporter,
+  exportIntervalMillis: 60000, // Export every 60 seconds (default is 60 seconds)
+});
+
 const sdk = new NodeSDK({
   resource: new Resource({
     [SEMRESATTRS_SERVICE_NAME]: 'otel-logger',
   }),
   traceExporter: new ConsoleSpanExporter(),
+  // traceExporter: jaegerExporter,
+  metricReader: metricReader,
 });
 
 if (process.env.NODE_ENV === 'production') {
