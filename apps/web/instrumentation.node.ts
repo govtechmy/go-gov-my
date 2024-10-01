6866f930f9e434dc07e6d96d513756984eaad976
yxchandev <chanyuxiang6@gmail.com>
2024-10-01 13:29:20 +0800

    remove deprecated imports

--- apps/web/instrumentation.node.ts
+++ apps/web/instrumentation.node.ts
@@ -2,13 +2,29 @@ import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import { Resource } from '@opentelemetry/resources';
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { SimpleSpanProcessor } from '@opentelemetry/sdk-trace-node';
-import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
+import {
+  SEMRESATTRS_SERVICE_INSTANCE_ID,
+  SEMRESATTRS_SERVICE_NAME,
+  SEMRESATTRS_SERVICE_NAMESPACE,
+} from '@opentelemetry/semantic-conventions';
+
+const otlpExporter = new OTLPTraceExporter({
+  url:
+    process.env.NODE_ENV === 'production'
+      ? 'https://jaeger:4318/v1/traces'
+      : 'http://localhost:4318/v1/traces',
+});
 
 const sdk = new NodeSDK({
+  // resource: new Resource({
+  //   [SemanticResourceAttributes.SERVICE_NAME]: 'gogov-web4',
+  // }),
   resource: new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: 'gogov-web2',
+    [SEMRESATTRS_SERVICE_NAME]: 'gogov-web',
+    [SEMRESATTRS_SERVICE_NAMESPACE]: 'gogov',
+    [SEMRESATTRS_SERVICE_INSTANCE_ID]: 'gogov-web',
   }),
-  spanProcessor: new SimpleSpanProcessor(new OTLPTraceExporter()),
+  spanProcessor: new SimpleSpanProcessor(otlpExporter),
 });
 sdk.start();
 
