04250388b783951f32929ab0b5df7e39811abadf
yxchandev <chanyuxiang6@gmail.com>
2024-10-04 10:28:46 +0800

    Update route.ts

--- apps/web/app/api/metrics/route.ts
+++ apps/web/app/api/metrics/route.ts
@@ -1,9 +1,6 @@
 import { promClient } from '@/lib/metrics/statuscode';
 import { NextResponse } from 'next/server';
 
-export const collectDefaultMetrics = promClient.collectDefaultMetrics;
-collectDefaultMetrics({ timeout: 5000 });
-
 export async function GET() {
   try {
     return NextResponse.json(await promClient.register.metrics(), {
