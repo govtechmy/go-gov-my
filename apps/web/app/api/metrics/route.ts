dd3f384ec1b241a3628a7b0426c40adb436caa8b
yxchandev <chanyuxiang6@gmail.com>
2024-10-18 12:45:42 +0800

    Update route.ts

--- apps/web/app/api/metrics/route.ts
+++ apps/web/app/api/metrics/route.ts
@@ -1,13 +1,16 @@
 import { counter200, register } from '@/lib/metrics/prom';
 import { NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
   try {
     console.log('1111', await counter200.get());
     const headers = new Headers({
       'Content-Type': register.contentType,
       'Cache-Control':
-        'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0', // Disable caching
+        'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0',
+      'x-nextjs-cache': 'MISS',
     });
     return new NextResponse(await register.metrics(), { headers });
   } catch (err) {
