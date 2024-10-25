12ef6c8c29549f60d48ea49e76c273d30a5496a4
PaanJoe <38974707+paanjoe@users.noreply.github.com>
2024-10-25 19:07:44 +0800

    fix kafka

--- apps/web/kafka-consumer/consumers/analyticConsumer.ts
+++ apps/web/kafka-consumer/consumers/analyticConsumer.ts
@@ -1,6 +1,6 @@
 import { prisma } from '@/lib/prisma';
 import { AnalyticsMessageSchema } from 'kafka-consumer/models/AnalyticsSchema';
-import type { Consumer, Logger } from 'kafkajs';
+import type { Consumer, EachMessagePayload, Logger } from 'kafkajs';
 import {
   consumeAnalytics,
   sumTwoObj,
@@ -16,116 +16,151 @@ export async function runAnalyticConsumer(consumer: Consumer, log: Logger) {
 
   await consumer.run({
     autoCommit: false,
-    eachMessage: async ({ topic, partition, message }) => {
-      const processAnalyticsMessage = async () => {
-        if (!message.value) return;
-
-        const data = await AnalyticsMessageSchema.parseAsync(
-          JSON.parse(message.value.toString('utf8') || '{}'),
-        );
-        const { aggregatedDate, from, to } = data;
-        const { idempotencyKey, hashedPayload } = toIdempotentResource(data);
-
-        // Check if the message has been processed before
-        const idempotentResource = await prisma.idempotentResource.findUnique({
-          where: { idempotencyKey },
-        });
-        if (idempotentResource !== null) {
-          if (idempotentResource.hashedPayload !== hashedPayload) {
-            throw new Error(
-              'Idempotent resource hashed payload does not match',
-            );
-          }
-          // Commit offset and return early if the message is already processed
-          await consumer.commitOffsets([
-            {
-              topic,
-              partition,
-              offset: (parseInt(message.offset, 10) + 1).toString(),
-            },
-          ]);
-          return;
-        }
-
-        await prisma.$transaction(async (tx) => {
-          const linkAnalyticsPromises = data.linkAnalytics.map(
-            async (analytics) => {
-              // 1. Get Analytics Rows
-              const row = await prisma.analytics.findMany({
-                where: {
-                  AND: [
-                    { aggregatedDate: new Date(aggregatedDate) },
-                    { linkId: { equals: analytics.linkId } },
-                  ],
-                },
-                take: 1,
-              });
-
-              if (row.length > 0) {
-                const metaDataFromDb = row[0]?.metadata || {};
-
-                // Sum metadata
-                const combineMetaData = sumTwoObj(
-                  metaDataFromDb,
-                  consumeAnalytics(
-                    analytics,
-                    new Date(aggregatedDate),
-                    from,
-                    to,
-                  ).metadata,
-                );
-
-                // Update records
-                await tx.analytics.update({
-                  where: { id: row[0].id },
-                  data: { metadata: combineMetaData, from, to },
-                });
-              } else {
-                // 3. If not exists, create a new record (meaning new day)
-                await tx.analytics.create({
-                  data: consumeAnalytics(
-                    analytics,
-                    new Date(aggregatedDate),
-                    from,
-                    to,
-                  ),
-                });
-              }
-
-              // Increment the link's clicks column with error handling
-              try {
-                await tx.link.update({
-                  where: { id: analytics.linkId },
-                  data: { clicks: { increment: analytics.total } },
-                });
-              } catch (error) {
-                console.error(
-                  `Failed to update clicks for linkId: ${analytics.linkId}`,
-                  error,
-                );
-              }
-            },
+    eachMessage: async (payload: EachMessagePayload) => {
+      try {
+        await processMessage(payload, consumer, log);
+      } catch (error) {
+        if (error.name === 'KafkaJSOffsetOutOfRange') {
+          const { topic, partition } = payload;
+          log.warn(
+            `Offset out of range for topic ${topic}, partition ${partition}. Resetting to earliest.`,
           );
+          await consumer.seek({ topic, partition, offset: '0' });
+          // Optionally, you can retry processing the message here
+          // await processMessage(payload, consumer, log);
+        } else {
+          log.error('Error processing message:', error);
+          // Handle other types of errors as needed
+        }
+      }
+    },
+  });
+}
 
-          await Promise.all(linkAnalyticsPromises);
+async function processMessage(
+  { topic, partition, message }: EachMessagePayload,
+  consumer: Consumer,
+  log: Logger,
+) {
+  try {
+    const processAnalyticsMessage = async () => {
+      if (!message.value) return;
 
-          // Save the idempotent resource
-          await tx.idempotentResource.create({
-            data: { idempotencyKey, hashedPayload },
-          });
-        });
+      const data = await AnalyticsMessageSchema.parseAsync(
+        JSON.parse(message.value.toString('utf8') || '{}'),
+      );
+      const { aggregatedDate, from, to } = data;
+      const { idempotencyKey, hashedPayload } = toIdempotentResource(data);
 
-        // Commit offset if success
+      // Check if the message has been processed before
+      const idempotentResource = await prisma.idempotentResource.findUnique({
+        where: { idempotencyKey },
+      });
+      if (idempotentResource !== null) {
+        if (idempotentResource.hashedPayload !== hashedPayload) {
+          throw new Error('Idempotent resource hashed payload does not match');
+        }
+        // Commit offset and return early if the message is already processed
         await consumer.commitOffsets([
           {
             topic,
             partition,
-            offset: (parseInt(message.offset, 10) + 1).toString(),
+            offset: (BigInt(message.offset) + BigInt(1)).toString(),
           },
         ]);
-      };
+        return;
+      }
 
-      retryWithDelay(processAnalyticsMessage);
-    },
-  });
+      await prisma.$transaction(async (tx) => {
+        const linkAnalyticsPromises = data.linkAnalytics.map(
+          async (analytics) => {
+            // Check if the link exists before attempting to update
+            const existingLink = await tx.link.findUnique({
+              where: { id: analytics.linkId },
+              select: { id: true },
+            });
+
+            if (!existingLink) {
+              console.log(
+                `Link with ID ${analytics.linkId} not found. Skipping update.`,
+              );
+              return; // Skip this iteration if the link doesn't exist
+            }
+
+            // 1. Get Analytics Rows
+            const row = await prisma.analytics.findMany({
+              where: {
+                AND: [
+                  { aggregatedDate: new Date(aggregatedDate) },
+                  { linkId: { equals: analytics.linkId } },
+                ],
+              },
+              take: 1,
+            });
+
+            if (row.length > 0) {
+              const metaDataFromDb = row[0]?.metadata || {};
+
+              // Sum metadata
+              const combineMetaData = sumTwoObj(
+                metaDataFromDb,
+                consumeAnalytics(analytics, new Date(aggregatedDate), from, to)
+                  .metadata,
+              );
+
+              // Update records
+              await tx.analytics.update({
+                where: { id: row[0].id },
+                data: { metadata: combineMetaData, from, to },
+              });
+            } else {
+              // 3. If not exists, create a new record (meaning new day)
+              await tx.analytics.create({
+                data: consumeAnalytics(
+                  analytics,
+                  new Date(aggregatedDate),
+                  from,
+                  to,
+                ),
+              });
+            }
+
+            // Increment the link's clicks column with error handling
+            try {
+              await tx.link.update({
+                where: { id: analytics.linkId },
+                data: { clicks: { increment: analytics.total } },
+              });
+            } catch (error) {
+              console.error(
+                `Failed to update clicks for linkId: ${analytics.linkId}`,
+                error,
+              );
+            }
+          },
+        );
+
+        await Promise.all(linkAnalyticsPromises);
+
+        // Save the idempotent resource
+        await tx.idempotentResource.create({
+          data: { idempotencyKey, hashedPayload },
+        });
+      });
+
+      // Commit offset if success
+      await consumer.commitOffsets([
+        {
+          topic,
+          partition,
+          offset: (BigInt(message.offset) + BigInt(1)).toString(),
+        },
+      ]);
+    };
+
+    await retryWithDelay(processAnalyticsMessage);
+  } catch (error) {
+    log.error('Error processing analytics message:', error);
+    throw error; // Re-throw the error to be caught by the outer try-catch
+  }
 }
