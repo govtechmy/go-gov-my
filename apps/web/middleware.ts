276ca6e45be364c3207b29ffbe6e43553cf263bf
PaanJoe <38974707+paanjoe@users.noreply.github.com>
2025-01-13 15:00:42 +0800

    fix: fixed routing issue

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -21,31 +21,31 @@ export const config = {
 export default async function middleware(req: NextRequest, ev: NextFetchEvent) {
   const { domain, path, key, pathWithoutLocale } = parse(req);
 
-  // Development
-  if (process.env.NEXT_PUBLIC_APP_ENV?.toLocaleLowerCase() === 'development') {
-    return NextResponse.next({ request: { headers: req.headers } });
-  }
-
-  // Production
-  if (process.env.NEXT_PUBLIC_APP_ENV?.toLocaleLowerCase() === 'production') {
-    return NextResponse.next({ request: { headers: req.headers } });
-  }
-
   // Staging auth
   if (process.env.NEXT_PUBLIC_APP_ENV?.toLocaleLowerCase() === 'staging') {
     const basicAuth = req.headers.get('authorization');
     if (basicAuth) {
       const authValue = basicAuth.split(' ')[1];
       const [user, password] = atob(authValue).split(':');
-      if (user === 'admin' && password === process.env.STAGING_AUTH_PASSWORD) {
-        return NextResponse.next({ request: { headers: req.headers } });
-      }
-    }
 
-    return new NextResponse('Auth required', {
-      status: 401,
-      headers: { 'WWW-Authenticate': `Basic realm="Secure Area"` },
-    });
+      if (user === 'admin' && password === process.env.STAGING_AUTH_PASSWORD) {
+        const response = NextResponse.next({
+          request: { headers: req.headers },
+        });
+        response.headers.set('Authorization', basicAuth);
+        // return response;
+      } else {
+        return new NextResponse('Auth required', {
+          status: 401,
+          headers: { 'WWW-Authenticate': `Basic realm="Secure Area"` },
+        });
+      }
+    } else {
+      return new NextResponse('Auth required', {
+        status: 401,
+        headers: { 'WWW-Authenticate': `Basic realm="Secure Area"` },
+      });
+    }
   }
 
   // for Admin
