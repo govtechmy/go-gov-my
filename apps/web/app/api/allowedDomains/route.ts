e9a8897bc0df6e8b7ea810d8ec77e3e26e2e0ec4
yxchandev <chanyuxiang6@gmail.com>
2025-02-19 15:21:11 +0800

    find -> findMany

--- apps/web/app/api/allowedDomains/route.ts
+++ apps/web/app/api/allowedDomains/route.ts
@@ -3,10 +3,10 @@ import { prisma } from '@/lib/prisma';
 
 export async function GET(request: NextRequest) {
   try {
-    const allowedDomains = await prisma.allowedDomains.find();
+    const allowedDomains = await prisma.allowedDomains.findMany();
 
     return NextResponse.json(allowedDomains);
   } catch (error) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
+    return NextResponse.json({ error: (error as Error).message }, { status: 500 });
   }
 }
