4a80366614fb7c3c52be63c55bba02344a6603a3
yxchandev <chanyuxiang6@gmail.com>
2025-02-19 18:26:46 +0800

    remove allowed domains file

--- apps/web/app/[locale]/app.go.gov.my/(auth)/login/allowedDomain.ts
+++
@@ -1,7 +0,0 @@
-// Farhan: Add this via database
-const allowedDomains = ['gov.my', 'moe.gov.my', 'mof.gov.my', 'moh.gov.my']; // Add all allowed domains here
-
-export const allowedDomain = (email: string, isLocal: boolean): boolean => {
-  if (isLocal) return true;
-  return allowedDomains.some((domain) => email.endsWith(`${domain}`));
-};
